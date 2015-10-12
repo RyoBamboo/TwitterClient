f5be2fc13515307d4e36cdda446ef0c9c5445532
ryo takenoshita <itakedaka@gmail.com>
2015-10-12 10:51:37 +0900

    スプラッシュ画面を起動時に表示

--- app/src/main/java/com/ryobamboo/twitterclient/activities/SplashScreenActivity.java
+++ app/src/main/java/com/ryobamboo/twitterclient/activities/SplashScreenActivity.java
@@ -1,4 +1,33 @@
 package com.ryobamboo.twitterclient.activities;
 
-public class SplashScreenActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.ryobamboo.twitterclient.MainActivity;
+import com.ryobamboo.twitterclient.R;
+
+public class SplashScreenActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SplashScreenActivity.this.finish();
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(intent);
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
 }
