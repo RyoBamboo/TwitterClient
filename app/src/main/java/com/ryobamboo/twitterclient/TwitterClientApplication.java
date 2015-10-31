cdb83c5a0668dd4b3ade9911ba0a1ddd5f8059fc
ryo takenoshita <itakedaka@gmail.com>
2015-10-31 16:58:22 +0900

    アプリ起動時にアプリケーションクラスを実行するように変更

--- app/src/main/java/com/ryobamboo/twitterclient/TwitterClientApplication.java
+++ app/src/main/java/com/ryobamboo/twitterclient/TwitterClientApplication.java
@@ -4,4 +4,23 @@ import android.app.Application;
 
 public class TwitterClientApplication extends Application {
 
+    private static TwitterClientApplication instance;
+
+    public TwitterClientApplication() {
+        instance = this;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    public static TwitterClientApplication getInstance() {
+        return instance;
+    }
 }
